697bf11770c6216d047a785f5199b7be6e5d6321
davidzonn <davidzonneveld@gmail.com>
2020-04-13 13:49:20 +0200

    summarize turnstile events with KSQL

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -32,7 +32,7 @@ class Turnstile(Producer):
         )
 
         super().__init__(
-            f"com.udacity.turnstile.{station_name}.entries",
+            f"com.udacity.turnstile.entries",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
